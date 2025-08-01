f9527aacd820d751d3cad65d59a269dbc7ccac23
Niklas Buschmann <niklasbuschmann@posteo.de>
2025-08-01 14:17:47 +0200

    use color: light-dark()

--- assets/js/toggle-color.js
+++ assets/js/toggle-color.js
@@ -1,16 +1,7 @@
 function toggleColor() {
-  for (var styleSheet of document.styleSheets) {
-    for (var rule of styleSheet.cssRules) {
-      if (rule.media && rule.media.mediaText.includes('prefers-color-scheme: light')) {
-        rule.media.deleteMedium('(prefers-color-scheme: light)');
-        rule.media.appendMedium('(prefers-color-scheme: dark)');
-      }
-      else if (rule.media && rule.media.mediaText.includes('prefers-color-scheme: dark')) {
-        rule.media.deleteMedium('(prefers-color-scheme: dark)');
-        rule.media.appendMedium('(prefers-color-scheme: light)');
-      }
-    }
-  }
+  var meta = document.querySelector('meta[name=color-scheme]');
+  var dark = meta.getAttribute('content') == 'light dark' ? matchMedia('(prefers-color-scheme: light)').matches : meta.getAttribute('content') == 'light';
+  meta.setAttribute('content', dark ? 'dark' : 'light');
 }
 
 function toggle() {
@@ -18,10 +9,10 @@ function toggle() {
     localStorage.removeItem('toggle');
   else
     localStorage.setItem('toggle', 1);
-  toggleColor()
+  toggleColor();
 }
 
 if (localStorage.getItem('toggle'))
   toggleColor();
 
-document.head.appendChild(document.createElement('style')).innerHTML = 'body > * {transition: .2s all ease-out}';
+document.head.innerHTML += '<style>body > * {transition: .2s all ease-out}</style>';
